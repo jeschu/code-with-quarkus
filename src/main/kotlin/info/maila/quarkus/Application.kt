97cfcaa6a8a7f032a9bc84051ca74e76a5beceb8
Jens Schumacher <jeschu@ok.de>
2020-10-31 00:21:52 +0100

    Postgres in Docker Container

--- src/main/kotlin/info/maila/quarkus/Application.kt
+++ src/main/kotlin/info/maila/quarkus/Application.kt
@@ -1,82 +1,61 @@
 package info.maila.quarkus
 
 import io.agroal.api.AgroalDataSource
-import io.quarkus.runtime.LaunchMode
-import io.quarkus.runtime.LaunchMode.DEVELOPMENT
 import io.quarkus.runtime.Quarkus.run
 import io.quarkus.runtime.Quarkus.waitForExit
 import io.quarkus.runtime.QuarkusApplication
 import io.quarkus.runtime.annotations.QuarkusMain
-import io.quarkus.runtime.configuration.ProfileManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 import mu.KotlinLogging
-import org.h2.tools.Server
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
+const val pgContainerName = "pg-code-with-quarkus"
+
 @QuarkusMain
-@ObsoleteCoroutinesApi
 class Application : QuarkusApplication {
+
+    init {
+        ProcessBuilder("docker", "run", "-d", "--rm",
+                "--name", pgContainerName,
+                "-p", "54320:5432",
+                "-e", "POSTGRES_DB=world",
+                "-e", "POSTGRES_USER=quarkus",
+                "-e", "POSTGRES_PASSWORD=world",
+                "postgres:13"
+        )
+                .start()
+                .waitFor()
+        TimeUnit.SECONDS.sleep(5)
+    }
 
     @Inject
     lateinit var dataSource: AgroalDataSource
 
     override fun run(vararg args: String?): Int {
 
-        h2DevMode()
+        dataSource.connection.use {conn ->
+            val meta = conn.metaData
+            logger.info { "Database: ${meta.databaseProductName} ${meta.databaseProductVersion}" }
+        }
 
         logger.info { "waitForExit() ..." }
         waitForExit()
         logger.info { "waitForExit() ... done" }
+
+        ProcessBuilder("docker", "rm", "-f", pgContainerName)
+                .start()
+                .waitFor()
+
         return 0
 
     }
 
-    private fun h2DevMode() {
-        go("H2 DEV mode configurator") {
-
-            val h2Logger = KotlinLogging.logger(name = "${logger.name}.H2-DEV")
-
-            val launchMode: LaunchMode = ProfileManager.getLaunchMode()
-            h2Logger.debug { "Launch Mode: $launchMode" }
-            if (launchMode == DEVELOPMENT) {
-
-                val h2TcpServer: Server = Server.createTcpServer().start()
-                h2Logger.debug { h2TcpServer.status }
-
-                val h2WebServer: Server = Server.createWebServer().start()
-                h2Logger.debug { h2WebServer.status }
-
-            }
-
-            if (h2Logger.isDebugEnabled) {
-                dataSource.connection.use {
-                    val meta = it.metaData
-                    h2Logger.debug { "Database: ${meta.databaseProductName} ${meta.databaseProductVersion}" }
-                }
-            }
-
-        }
-    }
-
-
 }
-
-@ObsoleteCoroutinesApi
-fun go(threadName: String? = null, block: CoroutineScope.() -> Unit) =
-    GlobalScope.launch(newSingleThreadContext(name = threadName ?: Thread.currentThread().name + "-go")) {
-        block()
-    }
 
 val logger = KotlinLogging.logger {}
 
-@ObsoleteCoroutinesApi
 fun main(args: Array<String>) {
 
-    logger.info { "startup 'code-with-quarkus' ..." }
     run(Application::class.java, *args)
 
 }
--- src/main/resources/db/migration/V1.0.0__world.sql
+++ src/main/resources/db/migration/V1.0.0__world.sql
@@ -1,10 +1,32 @@
 DROP TABLE IF EXISTS "city";
-CREATE TABLE "city"
-(
-    "ID"          int      NOT NULL DEFAULT SERIAL,
-    "Name"        char(35) NOT NULL DEFAULT '',
-    "CountryCode" char(3)  NOT NULL DEFAULT '',
-    "District"    char(20) NOT NULL DEFAULT '',
-    "Population"  int      NOT NULL DEFAULT 0,
-    PRIMARY KEY ("ID")
-)
+DROP TABLE IF EXISTS "country";
+
+CREATE TABLE "country" (
+    "Code" char(3) NOT NULL DEFAULT '',
+    "Name" char(52) NOT NULL DEFAULT '',
+    "Continent" char(20) NOT NULL DEFAULT 'Asia',
+    "Region" char(26) NOT NULL DEFAULT '',
+    "SurfaceArea" decimal(10,2) NOT NULL DEFAULT '0.00',
+    "IndepYear" smallint DEFAULT NULL,
+    "Population" int NOT NULL DEFAULT '0',
+    "LifeExpectancy" decimal(3,1) DEFAULT NULL,
+    "GNP" decimal(10,2) DEFAULT NULL,
+    "GNPOld" decimal(10,2) DEFAULT NULL,
+    "LocalName" char(45) NOT NULL DEFAULT '',
+    "GovernmentForm" char(45) NOT NULL DEFAULT '',
+    "HeadOfState" char(60) DEFAULT NULL,
+    "Capital" int DEFAULT NULL,
+    "Code2" char(2) NOT NULL DEFAULT '',
+    PRIMARY KEY ("Code")
+);
+
+CREATE TABLE "city" (
+    "ID" SERIAL NOT NULL,
+    "Name" CHAR(35) NOT NULL DEFAULT '',
+    "CountryCode" CHAR(3) NOT NULL DEFAULT '',
+    "District" CHAR(20) NOT NULL DEFAULT '',
+    "Population" INT NOT NULL DEFAULT 0,
+    PRIMARY KEY ("ID"),
+    CONSTRAINT "city2country" FOREIGN KEY ("CountryCode") REFERENCES "country"
+);
+
